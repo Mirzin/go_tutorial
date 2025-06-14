81cd796b09483a0515da3c015e20d8858e6b2859
Mirzin <mirzinsheroqueabdullah@gmail.com>
2025-06-14 20:40:09 +0530

    GTG outside so stopping at 1:04:07

---
+++ internal/middleware/authorization.go
@@ -0,0 +1,10 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/Mirzin/go_tutorial/api"
+	"github.com/Mirzin/go_tutorial/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
